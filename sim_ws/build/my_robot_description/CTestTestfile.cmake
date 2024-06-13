e6eeb4a3f8930550c599fddac331ccc447c9da0b
HaydenYe <yetwhen@gmail.com>
2024-06-13 03:22:00 -0400

    created a new workspace for gazebo rviz urdf simulation, created a my_robot_description package which install the urdf into correct place in ws, implemented launch files in python and xml each which publishes urdf&joint info and launch rviz configuration saved

---
+++ sim_ws/build/my_robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hayden/ROS2_demo/sim_ws/src/my_robot_description
+# Build directory: /home/hayden/ROS2_demo/sim_ws/build/my_robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
